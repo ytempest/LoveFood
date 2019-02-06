f5111daa277dc9c38f1f185a0e7b753f7aa912cf
ytempest <787491096@qq.com>
2019-02-06 22:30:26 +0800

    完成个人中心的部分逻辑

--- app/src/main/java/com/ytempest/lovefood/presenter/PersonalPresenter.java
+++ app/src/main/java/com/ytempest/lovefood/presenter/PersonalPresenter.java
@@ -3,7 +3,9 @@ package com.ytempest.lovefood.presenter;
 import com.ytempest.baselibrary.base.mvp.BasePresenter;
 import com.ytempest.baselibrary.base.mvp.inject.InjectModel;
 import com.ytempest.lovefood.contract.PersonalContract;
+import com.ytempest.lovefood.data.UserInfo;
 import com.ytempest.lovefood.model.PersonalModel;
+import com.ytempest.lovefood.util.UserHelper;
 
 /**
  * @author ytempest
@@ -11,4 +13,8 @@ import com.ytempest.lovefood.model.PersonalModel;
  */
 @InjectModel(PersonalModel.class)
 public class PersonalPresenter extends BasePresenter<PersonalContract.PersonalView, PersonalContract.Model> implements PersonalContract.Presenter {
+    @Override
+    public UserInfo getUserInfo() {
+        return UserHelper.getInstance().getUserInfo();
+    }
 }
